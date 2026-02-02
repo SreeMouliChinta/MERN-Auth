cde88900fca24a35d5807c3029496cc02b01b842
Sree Mouli Chinta <kanna.dec11@gmail.com>
2026-02-02 22:36:22 +0530

    update cors for first deployment

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+// const allowedOrigins = ["http://localhost:5173"];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // API Endpoints
 app.get("/", (req, res) => {
