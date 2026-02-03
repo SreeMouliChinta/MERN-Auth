cfa0f4fde43110d1e02a4d6fdea28a91c96737e2
Sree Mouli Chinta <kanna.dec11@gmail.com>
2026-02-03 22:41:51 +0530

    gmail transporter fix

--- server/config/transporter.js
+++ server/config/transporter.js
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, // TLS
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
