6e85f64697d6ff684b0cf2eac3a5219e3cc3af32
Sree Mouli Chinta <kanna.dec11@gmail.com>
2026-02-03 13:36:57 +0530

    fix(cors): update allowed origins with production frontend URL

--- server/server.js
+++ server/server.js
@@ -10,15 +10,18 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-// const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // API Endpoints
 app.get("/", (req, res) => {
   res.send("API Working");
+});
+app.get("/health", (req, res) => {
+  res.json({ success: "ok" });
 });
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
